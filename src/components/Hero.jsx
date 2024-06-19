dc6f0d5307210ef32e4acdb527c447958243428f
Aleksandra Kolesnikova <52916334+AleKoles@users.noreply.github.com>
2024-06-19 16:24:12 +0200

    restyle menu and create video hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import { Typed } from 'react-typed';
-
+import video from '../assets/tiffanyHero.mp4';
+import fallbackImage from '../assets/diamond.jpg';
+import { Link } from 'react-scroll';
 
 
 const Hero = () => {
   return (
     <div className='text-theme-gray' id="hero">
-      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-        <h1 className='text-theme-yellow md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
-          Grow with data.
-        </h1>
-        <div className='flex justify-center items-center'>
-          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
-            Fast, flexible financing for
-          </p>
+      <div class="relative w-full  md:w-10/12 mx-auto">
+        <video 
+          class="mt-24"
+          src={video} 
+          autoPlay 
+          muted 
+          loop 
+          poster={fallbackImage}
+        >
+          Your browser does not support the video tag.
+        </video>
+        <div class="absolute bg-black/20 z-20 bottom-0 left-0 h-full w-full flex flex-col justify-end items-center">
+          <div class="text-white flex flex-col gap-8 px-4 w-1/2 pb-8 xl:pb-16">
+            <h2 class="cormorant text-4xl text-center">SpiritStone by Myronenko</h2>
+            <p class="text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+            <Link class="rounded-full uppercase bg-white text-theme-blue2 w-fit px-4 py-2 mx-auto cursor-pointer hover:text-theme-blue1" to="contact" spy={true} smooth={true} offset={50} duration={500}>Contact us</Link>
+          </div>
         </div>
-       
-        <button className='bg-theme-yellow w-[200px] rounded-full font-medium my-6 mx-auto py-3 text-white'>Get Started</button>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import { Link } from 'react-scroll';
-
+import { TfiInstagram } from "react-icons/tfi";
+import { PiFacebookLogoLight } from "react-icons/pi";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -11,25 +12,28 @@ const Navbar = () => {
   };
 
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-theme-gray'>
-      <Link to="hero" spy={true} smooth={true} offset={50} duration={500}><h1 className='cursor-pointer w-full text-xl font-semibold text-theme-gray'>Stella Accounting</h1></Link>
-      <ul className='hidden md:flex'>
-        <li className='p-4'><Link to="about" spy={true} smooth={true} offset={50} duration={500}>About</Link></li>
-        <li className='p-4'>Resources</li>
-        <li className='p-4'>About</li>
-        <li className='p-4'>Contact</li>
-      </ul>
-      <div onClick={handleNav} className='block md:hidden'>
-          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
-      </div>
-      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-theme-yellow m-4'>REACT.</h1>
-          <li className='p-4 border-b border-gray-600'>Home</li>
-          <li className='p-4 border-b border-gray-600'><Link>Company</Link></li>
-          <li className='p-4 border-b border-gray-600'>Resources</li>
-          <li className='p-4 border-b border-gray-600'>About</li>
-          <li className='p-4'>Contact</li>
-      </ul>
+    <div className='flex fixed top-0 z-50 bg-white/80 backdrop-blur-lg  w-full px-4 border-t-4 border-theme-blue1'>
+      <nav class="justify-between w-full flex items-center h-24 max-w-7xl mx-auto">
+        <ul className='hidden md:flex gap-4 items-center justify-start text-theme-gray2 basis-1/3'>
+            <li className='hover:text-black duration-150 ease-in-out cursor-pointer'><TfiInstagram size={20} /></li>
+            <li className='hover:text-black duration-150 ease-in-out cursor-pointer'><PiFacebookLogoLight size={27} /></li>
+        </ul>   
+        <Link to="hero" spy={true} smooth={true} offset={50} duration={500}><h1 className='basis-1/3 cursor-pointer w-full text-3xl font-semibold text-theme-gray cormorant'>SpiritStone</h1></Link>
+        <ul className='hidden md:flex items-center justify-end text-theme-gray2 gap-4 basis-1/3'>
+          <li className='group relative hover:text-black duration-150 ease-in-out tracking-wide cursor-pointer'><Link to="about" spy={true} smooth={true} offset={50} duration={500}>About</Link><span class="absolute bottom-0 left-0 w-0 group-hover:w-full border-t-2 border-theme-blue1 transition-all duration-150"></span></li>
+          <li className='group relative hover:text-black duration-150 ease-in-out tracking-wide cursor-pointer'><Link to="products" spy={true} smooth={true} offset={50} duration={500}>Products</Link><span class="absolute bottom-0 left-0 w-0 group-hover:w-full border-t-2 border-theme-blue1 transition-all duration-150"></span></li>
+          <li className='group relative hover:text-black duration-150 ease-in-out traking-wide cursor-pointer'><Link to="contact" spy={true} smooth={true} offset={50} duration={500}>Contact</Link><span class="absolute bottom-0 left-0 w-0 group-hover:w-full border-t-2 border-theme-blue1 transition-all duration-150"></span></li>
+        </ul>
+        <div onClick={handleNav} className='block md:hidden'>
+            {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+        </div>
+        <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+            <li className='p-4 border-b border-gray-600'>About</li>
+            <li className='p-4 border-b border-gray-600'><Link>Products</Link></li>
+            <li className='p-4 border-b border-gray-600'>Contact</li>
+        </ul>
+      </nav>
+     
     </div>
   );
 };
