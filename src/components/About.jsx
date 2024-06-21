d6010a804ed3906a2d2d932748f2cc354a2a179e
Alexandra <kolesnikova@lux-medien.com>
2024-06-21 21:34:01 +0200

    add some client logos/fix pagination in gallery

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,9 +9,9 @@ import { GiGemPendant } from "react-icons/gi";
 const About = () => {
   return (
     <div className='w-full bg-white py-24 px-4' id="about">
-      <div className='max-w-7xl w-full border-y-4 border-white mx-auto flex gap-12 flex-col'>
+      <div className='max-w-7xl w-full mx-auto flex gap-12 flex-col'>
         <h2 class="w-full text-center text-3xl sm:text-5xl cormorant font-bold">A gem cutting family business since 1995</h2>
-        <div className='w-full grid grid-cols-1 md:grid-cols-3 gap-8 items-srtrt justify-between'>
+        <div className='w-full grid grid-cols-1 md:grid-cols-3 gap-8 items-start justify-between'>
           <p class="col-span-full lg:text-lg">With our deep understanding of each gemstone's intricacies, we proudly offer an extensive selection available for your order. We collaborate closely with companies and individual clients with a personal touch.</p>
           <div className="flex flex-col gap-2 items-center"> 
             <GiGems size={45} className="text-theme-blue2"/>
