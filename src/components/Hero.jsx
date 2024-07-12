105ea87105336c1ec393ed3236f53a2841890ad7
Aleksandra Kolesnikova <52916334+AleKoles@users.noreply.github.com>
2024-07-12 16:09:11 +0200

    compres videos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,32 +9,33 @@ const Hero = () => {
   return (
     <div className='text-theme-gray' id="hero">
       <div className="relative w-full">
-        <video
-          className="w-full aspect-video mt-12 object-cover hidden sm:block"
-          autoPlay
-          muted
-          loop
-          playsInline 
-          disableRemotePlayback
-          preload="auto" 
-          poster={fallbackImageD}
-        >
-          <source src={headerDesktop} type="video/mp4" /> 
-          Your browser does not support the video tag.
-        </video>
-        <video
-          className="mt-24 w-full h-auto object-cover sm:hidden"
-          autoPlay
-          muted
-          loop
-          playsInline 
-          disableRemotePlayback
-          preload="auto" 
-          poster={fallbackImageM}
-        >
-          <source src={headerMobile} type="video/mp4" /> 
-          Your browser does not support the video tag.
-        </video>
+      <video
+  className="w-full aspect-video mt-12 object-cover hidden sm:block"
+  autoPlay
+  muted
+  loop
+  playsInline 
+  disableRemotePlayback
+  preload="auto" 
+  poster={fallbackImageD}
+>
+  <source src={headerDesktop} type="video/mp4" /> 
+  Your browser does not support the video tag.
+</video>
+<video
+  className="mt-24 w-full aspect-square object-cover sm:hidden"
+  autoPlay
+  muted
+  loop
+  playsInline 
+  disableRemotePlayback
+  preload="auto" 
+  poster={fallbackImageM}
+>
+  <source src={headerMobile} type="video/mp4" /> 
+  Your browser does not support the video tag.
+</video>
+
         <div className="absolute bg-black/10 z-20 bottom-0 left-0 h-full w-full flex flex-col justify-end items-center">
           <div className="text-white flex flex-col gap-8 px-4 w-full sm:w-1/2 pb-4 sm:pb-8 xl:pb-28">
             <h1 className="cormorant text-5xl sm:text-6xl text-center">SpiritStone</h1>
